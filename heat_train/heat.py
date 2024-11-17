04cdcecb7a2934c09842af5fd87d4a58e40b9da7
zf-wei <zfwei11@gmail.com>
2024-11-16 20:36:37 -0800

    添加了 Modified DeepONet

--- heat_train/heat.py
+++ heat_train/heat.py
@@ -13,9 +13,13 @@ from torch.utils.data import Dataset
 import argparse
 parser = argparse.ArgumentParser(description="DeepONet with configurable parameters.")
 parser.add_argument('--var', type=int, default=0, help='Variant of DeepONet')
+parser.add_argument('--struct', type=int, default=2, help='Structure of DeepONet')
+parser.add_argument('--boundary_parameter', type=float, default=0, help='Weight parameter for boundary conditions')
 # 解析命令行参数
 args = parser.parse_args()
 var = args.var
+struct = args.struct
+boundary_parameter = args.boundary_parameter
 
 
 time_limit = 1
@@ -25,15 +29,33 @@ total_time_steps = int(time_limit/time_step)
 total_sample = 500
 boundary = int(total_sample*4/5) # 设置训练集和测试集的边界
 batch_size = 20
-hidden_dims = [200,200,200,200]
-output_dim = 50
 epochs = 3000
 
 # Hyperparameters
 branch_input_dim = n_points  # Number of points to represent the original function
 trunk_input_dim = 2     # Coordinate where we evaluate the transformed function
-###############################################################################
 
+# Define the dictionary mapping struct values to neural network structures
+if var!=6:
+    structures = {
+        1: {'hidden_dims': [100, 100, 100, 100], 'output_dim': 50},
+        2: {'hidden_dims': [200, 200, 200, 200], 'output_dim': 50}
+    }
+
+    # Get the configuration based on the struct value
+    config = structures.get(struct, {'hidden_dims': [], 'output_dim': 0})
+
+    hidden_dims = config['hidden_dims']
+    output_dim = config['output_dim']
+elif var==6:
+    structure_params = {
+        1: (3, 3, 100, 50),
+        2: (3, 3, 200, 50),
+    }
+    if struct in structure_params:
+        branch_depth, trunk_depth, hidden_dim, output_dim = structure_params[struct]
+    else:
+        raise ValueError("Invalid structure type")
 
 #%%
 # In this cell, we define the function to get the cell centers of a 1D mesh. 
@@ -217,7 +239,6 @@ class TrunkNet(nn.Module):
         return self.fc(y)
 
 #%%
-# DeepONet Variants
 # In this cell, we define the DeepONet Variants
 class DeepONet_0(nn.Module):
     def __init__(self, branch_input_dim, trunk_input_dim, hidden_dims, output_dim):
@@ -302,16 +323,124 @@ class DeepONet_4(nn.Module):
         output = torch.sum(branch_output * trunk_output, dim=-1, keepdim=True)  # 按照最后一个坐标做内积
         return output
 
-DeepONets = [DeepONet_0, DeepONet_1, DeepONet_2, DeepONet_3, DeepONet_4]
 #%%
-# Define the loss function
+# In this cell, we define the DeepONet_6, the modified DeepONet
+# Branch Network
+class MBranchNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, depth):
+        super(MBranchNet, self).__init__()
+        self.elu = nn.ELU()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+
+        # 动态生成多层隐藏层
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for _ in range(depth)])
+
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+        # Apply weight initialization if needed
+        # self.apply(initialize_weights)
+
+    def forward(self, x, U, V):
+        xx = self.elu(self.fc1(x))
+        xx = torch.mul(1 - xx, U) + torch.mul(xx, V)
+
+        # 动态遍历隐藏层
+        for hidden_layer in self.hidden_layers:
+            xx = self.elu(hidden_layer(xx))
+            xx = torch.mul(1 - xx, U) + torch.mul(xx, V)
+
+        return self.fc_out(xx)
+
+
+# Trunk Network
+class MTrunkNet(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, depth):
+        super(MTrunkNet, self).__init__()
+        self.elu = nn.ELU()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+
+        # 动态生成多层隐藏层
+        self.hidden_layers = nn.ModuleList([nn.Linear(hidden_dim, hidden_dim) for _ in range(depth)])
+
+        self.fc_out = nn.Linear(hidden_dim, output_dim)
+
+        # Apply weight initialization if needed
+        # self.apply(initialize_weights)
+
+    def forward(self, y, U, V):
+        yy = self.elu(self.fc1(y))
+        yy = torch.mul(1 - yy, U) + torch.mul(yy, V)
+
+        # 动态遍历隐藏层
+        for hidden_layer in self.hidden_layers:
+            yy = self.elu(hidden_layer(yy))
+            yy = torch.mul(1 - yy, U) + torch.mul(yy, V)
+
+        return self.fc_out(yy)
+
+
+# DeepONet_6
+class DeepONet_6(nn.Module):
+    def __init__(self, branch_input_dim, branch_depth, trunk_input_dim, trunk_depth, hidden_dim, output_dim):
+        super(DeepONet_6, self).__init__()
+        self.elu = nn.ELU()
+        self.branch_net = MBranchNet(branch_input_dim, hidden_dim, output_dim, branch_depth)
+        self.trunk_net = MTrunkNet(trunk_input_dim, hidden_dim, output_dim, trunk_depth)
+        self.fcB = nn.Linear(branch_input_dim, hidden_dim)
+        self.fcT = nn.Linear(trunk_input_dim, hidden_dim)
+        # Apply weight initialization
+        # self.apply(initialize_weights)
+
+    def forward(self, x, y):
+        U = self.elu(self.fcB(x))
+        V = self.elu(self.fcT(y))
+
+        branch_output = self.branch_net(x, U, V)
+        trunk_output = self.trunk_net(y, U, V)
+        # Combine the outputs (typically element-wise product)
+
+        output = torch.sum(branch_output * trunk_output, dim=-1, keepdim=True)  # 按照最后一个坐标做内积
+        return output
+
+#%%
+# In this cell, we define the dictionary mapping the variant number to the corresponding DeepONet model
+DeepONets = {
+    0: DeepONet_0,
+    1: DeepONet_1,
+    2: DeepONet_2,
+    3: DeepONet_3,
+    4: DeepONet_4,
+    6: DeepONet_6
+}
+#%%
+# In this cell, we define the loss function
+
+# mean squared error function
 def mse(prediction, target):
     ms_loss = torch.mean((prediction - target) ** 2)
     return ms_loss
+# boundary condition
+def boundary_error(prediction):
+    prediction.reshape(-1, total_time_steps, n_points)
+    left_boundary = prediction[:, :, 0]
+    right_boundary = prediction[:, :, -1]
+    both_boundary = torch.cat([left_boundary, right_boundary], dim=1)
+    both_boundary_squared = both_boundary ** 2
+    return both_boundary_squared.mean()
+# loss function
+def loss_fn(prediction, target, boundary_parameter=0):
+    ms_loss = mse(prediction, target)
+    bc_loss = boundary_error(prediction)
+    return ms_loss + boundary_parameter * bc_loss
 #%%
 # Create model
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = DeepONets[var](branch_input_dim, trunk_input_dim, hidden_dims, output_dim).to(device)
+
+if var!=6:
+    model = DeepONets[var](branch_input_dim, trunk_input_dim, hidden_dims, output_dim).to(device)
+elif var==6:
+    model = DeepONets[var](branch_input_dim, branch_depth, trunk_input_dim, trunk_depth, hidden_dim, output_dim).to(device)
+
 optimizer = optim.Adamax(model.parameters(), lr=0.001)
 #%%
 # 训练模型
@@ -332,13 +461,13 @@ for epoch in range(epochs):
 
         optimizer.zero_grad()
         outputs = model(input1_batch, input2_batch)
-        loss = mse(outputs, target_batch)
+        loss = loss_fn(outputs, target_batch, boundary_parameter)
         err.append(loss.item())
         if loss.item()<err_best:
             err_best = loss.item()
             best_epoch = epoch
             model_best = model.state_dict().copy()
-            model_filename_best = f"Var{var}_Sensor{n_points}_Batch{batch_size}-best.pth"
+            model_filename_best = f"Var{var}_Struct{struct}_Sensor{n_points}_Batch{batch_size}-best.pth"
             torch.save(model_best, model_filename_best)
             print(f"A best model at epoch {epoch+1} has been saved with training error {err_best:.9f}.", file=sys.stderr)
         loss.backward()
@@ -351,8 +480,8 @@ for epoch in range(epochs):
     err_prev = err_curr
     if epoch%50==49:
         # 保存损失值和模型，修改文件名以包含参数信息  
-        output_filename = f"Var{var}_Sensor{n_points}_Batch{batch_size}-final.npy"
-        model_filename = f"Var{var}_Sensor{n_points}_Batch{batch_size}-final.pth"
+        output_filename = f"Var{var}_Struct{struct}_Sensor{n_points}_Batch{batch_size}-final.npy"
+        model_filename = f"Var{var}_Struct{struct}_Sensor{n_points}_Batch{batch_size}-final.pth"
         np.save(output_filename, np.array(error_list))
         torch.save(model.state_dict(), model_filename)
         print(f"Model saving checkpoint: the model trained after epoch {epoch+1} has been saved with the training errors.", file=sys.stderr)
@@ -363,8 +492,8 @@ print(np.mean(errs,axis=1))
 '''
 #%%
 # 保存损失值和模型，修改文件名以包含参数信息
-output_filename = f"Var{var}_Sensor{n_points}_Batch{batch_size}-final.npy"
-model_filename = f"Var{var}_Sensor{n_points}_Batch{batch_size}-final.pth"
+output_filename = f"Var{var}_Struct{struct}_Sensor{n_points}_Batch{batch_size}-final.npy"
+model_filename = f"Var{var}_Struct{struct}_Sensor{n_points}_Batch{batch_size}-final.pth"
 
 np.save(output_filename, np.array(error_list))
 torch.save(model.state_dict(), model_filename)
--- heat_train/heat_train.sh
+++ heat_train/heat_train.sh
@@ -9,7 +9,7 @@
 #SBATCH --gres=gpu:1                     # Number of GPUs per node
 #SBATCH --time=8:00:00                  # Time limit hrs:min:sec
 #SBATCH -A ascr_dpdf                     # Account name (ascr_dpdf)
-#SBATCH --array=2-3                      # Array range for parameter combinations
+#SBATCH --array=0-4,6                      # Array range for parameter combinations
 
 
 # Activate Conda environment
@@ -17,4 +17,4 @@ source /share/apps/python/miniconda4.12/etc/profile.d/conda.sh
 conda activate test
 
 # Run the Python script with the current set of parameters
-python heat.py --var ${SLURM_ARRAY_TASK_ID}
+python heat.py --var ${SLURM_ARRAY_TASK_ID} --structure 1 --boundary_parameter 0
